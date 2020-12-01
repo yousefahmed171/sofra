7e20368d46e3bac7cb7351d7727998441118e092
yousefahmed171 <yousefahmed171@gmail.com>
2020-12-01 08:46:36 +0200

    add new file seed in function seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+         $this->call([
+            categoriesSedder::class,
+            citiesSedder::class,
+            regionsSedder::class
+         ]);
     }
 }
